b51e9929746ec83d070a64113caafdda33e31074
Rune Alexander Laursen <runeal17@uia.no>
2019-03-31 23:10:10 +0200

    added background to window-frame

--- src/View.java
+++ src/View.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class View extends JFrame {
 
@@ -6,10 +7,27 @@ public class View extends JFrame {
 
     public View(){
 
-        this.setSize(850,850);
+        this.setSize(900,900);
         this.setTitle("Blowout TD");
+        ImageIcon imageIcon = new ImageIcon("Pictures/Background.png");
+        Image image = imageIcon.getImage();
+        Image newimg = image.getScaledInstance(900, 900, Image.SCALE_SMOOTH);
+        imageIcon = new ImageIcon(newimg);
+
+        //JLabel background = new ImageIcon(newimg);
+
+        JLabel background = new JLabel(imageIcon);
+        add(background);
+
 
         this.setVisible(true); //MÅ VÆRE SISTE LINJE!
 
     }
 }
+
+
+//        this.setTitle("Blowout TD");
+//        JLabel background = new JLabel(new ImageIcon("Pictures/Background.png"));
+//
+//        add(background);
+
