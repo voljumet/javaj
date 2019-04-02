ff34a5d3ae84fc81c1d2bc9bd7b27f6a7f4595f4
Morteza Haidari <morteh18@student.uia.no>
2019-04-02 15:46:51 +0200

    Added menu functionality and View class

--- src/View.java
+++ src/View.java
@@ -1,26 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import menuPackage.Menu;
 
-public class View extends JFrame {
+public class View{
 
-    JPanel MainGameWindow;
+    Menu menu;
+//    // Declaring the Java frame
+//    JFrame frame = new JFrame();
 
     public View(){
+//
+//        ImageIcon imageIcon = new ImageIcon("Pictures/Background.png");
+//        Image image = imageIcon.getImage();
+//        Image newimg = image.getScaledInstance(900, 800, Image.SCALE_SMOOTH);
+////        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        imageIcon = new ImageIcon(newimg);
+//
+//        JLabel background = new JLabel(imageIcon);
 
-        this.setSize(900,900);
-        this.setTitle("Blowout TD");
-        ImageIcon imageIcon = new ImageIcon("Pictures/Background.png");
-        Image image = imageIcon.getImage();
-        Image newimg = image.getScaledInstance(900, 900, Image.SCALE_SMOOTH);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        imageIcon = new ImageIcon(newimg);
-        this.setResizable(false);
-
-        JLabel background = new JLabel(imageIcon);
-        add(background);
-
-
-        this.setVisible(true); //MÅ VÆRE SISTE LINJE!
+        menu = new Menu();
+//        add(background);
 
     }
 }
