8e7aca5816c20051ac336ee8c53ee98adbd64028
Morteza Haidari <morteh18@student.uia.no>
2019-04-09 15:46:00 +0200

    Fixed two new InternalFrame

--- src/View.java
+++ src/View.java
@@ -1,48 +1,101 @@
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.concurrent.Flow;
 
 public class View extends JFrame {
 
-//    Menu menu;
-//    BackgroundImage backgroundImage;
+    //    // Declaring the Java parentFrame
+    static JFrame parentFrame = new JFrame("Towerdefence");
 
-    //    // Declaring the Java frame
-    JFrame frame = new JFrame();
     // Declaring Jmenubar, Jmenu, and JmenuItem.
     JMenuBar menuBar;
     JMenu menu;
     JMenuItem menuNewGame, menuSave, menuExit;
 
+    // label to display text
+    static JLabel label1, label2;
+
 
     public View(){
 
-//        menu = new Menu();
-//        backgroundImage = new BackgroundImage();
+        super("Making an internalFrame");
 
-        // Adding frame functionalities.
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setSize(900,800);
-        frame.setTitle("Oil Platform Defence");
-        frame.setLocationRelativeTo(null);
+        // Adding parentFrame functionalities.
+        parentFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        parentFrame.setLayout(new BorderLayout());
+//        parentFrame.setSize(900,800);
+        parentFrame.setTitle("Oil Platform Defence");
+        parentFrame.setLocationRelativeTo(null);
 
+
+        /* Creating the internalframe */
+
+        // creating a new frame
+        parentFrame = new JFrame("frame");
+        parentFrame.setUndecorated(true);
+
+        // setting layout of frame
+        parentFrame.setLayout(new FlowLayout());
+
+        // creating a internal frame, with borders and titlebar set to null
+        JInternalFrame internalFrameButtons = new JInternalFrame(null, false, false, false, false);
+        internalFrameButtons.putClientProperty("JInternalFrame.isPalette", Boolean.TRUE);
+        internalFrameButtons.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+        ((javax.swing.plaf.basic.BasicInternalFrameUI) internalFrameButtons.getUI()).setNorthPane(null);
+        internalFrameButtons.setBorder(null);
+        internalFrameButtons.setPreferredSize(new Dimension(180, 900));
+
+        // creating game internal frame, with borders and titlebar set to null
+        JInternalFrame internalFrameGame = new JInternalFrame(null, false, false, false, false);
+        internalFrameGame.putClientProperty("JInternalFrame.isPalette", Boolean.TRUE);
+        internalFrameGame.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+        ((javax.swing.plaf.basic.BasicInternalFrameUI) internalFrameButtons.getUI()).setNorthPane(null);
+//        internalFrameGame.setUI(null); // dersom du vil slette alt i internalframeGame
+        internalFrameGame.setBorder(null);
+        internalFrameGame.setPreferredSize(new Dimension(900, 900));
+
+        /* End */
+
+        // creating a label to display text
+        label1 = new JLabel();
+        label2 = new JLabel();
+
+        // creating a panel
+        JPanel leftPanel = new JPanel();
+        JPanel rightPanel = new JPanel();
+
+        // adding label and button
+        leftPanel.add(label1);
+//        leftPanel.add(leftButton);
+
+        // panel2
+        rightPanel.add(label2);
+//        rightPanel.add(rightButton);
+
+
+        // creating buttons
+//        JButton leftButton = new JButton("Left button");
+//        JButton rightButton = new JButton("Right button");
+
+
+        /* InternalframeGame backgorund */
         // Adding background image
         ImageIcon imageIcon = new ImageIcon("Pictures/Background.png");
         Image image = imageIcon.getImage();
-        Image newImg = image.getScaledInstance(900, 800, Image.SCALE_SMOOTH);
+        Image newImg = image.getScaledInstance(900, 865, Image.SCALE_SMOOTH);
         imageIcon = new ImageIcon(newImg);
 
         JLabel background = new JLabel(imageIcon);
-        frame.add(background);
-
+        rightPanel.add(background);
 
         // creating menu with bar and sub menu
         menuBar = new JMenuBar();
-        frame.setJMenuBar(menuBar); // frame to set our menubar to the frame.
+//        parentFrame.setJMenuBar(menuBar); // parentFrame to set our menubar to the parentFrame.
 
         // menu naming, and functionality
         menu = new JMenu("Game");
@@ -56,12 +109,14 @@ public class View extends JFrame {
         menuNewGame = new JMenuItem("New Game");
         menu.add(menuNewGame);
         menu.addSeparator();
+        menuSave = new JMenuItem("Save game");
+        menu.add(menuSave);
+        menu.addSeparator();
         menuExit = new JMenuItem("Exit Game");
         menu.add(menuExit);
 
 
-
-        //Defining new popup frame
+        //Defining new popup parentFrame
         final JFrame popFrame;
 
         //Menu
@@ -76,7 +131,7 @@ public class View extends JFrame {
         popupItem3 = new JMenuItem("Exit");
 
 
-        // Jpopupmenu
+        /*JpopupMenu*/
         popFrame = new JFrame();
         popupMenu = new JPopupMenu();
 
@@ -94,18 +149,42 @@ public class View extends JFrame {
 
             }
         });
+        
+        
 
         popFrame.add(popupMenu);
         popFrame.setSize(300,300);
         popFrame.setLayout(null);
         popFrame.setLocationRelativeTo(null);
 
+        /* JpopupMenu */
 
-        // Setting frame visible/ not resizeable.
-        frame.setResizable(false);
-        frame.setVisible(true);
 
-        // The popupframe to be visible after the frame
+        // adding panel to internal frame
+        internalFrameButtons.add(leftPanel);
+//        internalFrameButtons.setUndecorated();
+        internalFrameGame.add(rightPanel);
+
+
+        // adding internal frame to frame
+        parentFrame.add(internalFrameButtons, BorderLayout.WEST);
+        parentFrame.add(internalFrameGame, BorderLayout.EAST);
+
+
+        // setting visibility internal frame
+        internalFrameButtons.setVisible(true);
+        internalFrameGame.setVisible(true);
+
+        // parentFrame.setSize(400,400);
+        parentFrame.setSize(1100,900);
+
+        parentFrame.show();
+        
+        // Setting parentFrame visible/ not resizeable.
+        parentFrame.setResizable(false);
+        parentFrame.setVisible(true);
+
+        // The popupframe to be visible after the parentFrame
         popFrame.setVisible(true);
 
     }
